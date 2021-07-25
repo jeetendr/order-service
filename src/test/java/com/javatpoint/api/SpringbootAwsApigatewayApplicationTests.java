51e65d22dd11a2a18cbf57e2255aded01ddafcf5
abc <abc@example.com>
2021-07-25 16:38:39 +0530

    first commit

---
+++ src/test/java/com/javatpoint/api/SpringbootAwsApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
